9e91f7d6fede8400e5f01a539baf2341b3ab01df
nardost <nardos.tessema@gmail.com>
2022-04-16 16:52:36 -0500

    Some links written (Just placeholders except the main page link)

---
+++ src/pages/layout/links.js
@@ -0,0 +1,7 @@
+export const navLinks = [{ label: "Launches", to: "/" }];
+
+export const otherLinks = [
+  { label: "Profile", to: "/404" },
+  { label: "Account", to: "/404" },
+  { label: "Dashboard", to: "/404" },
+];
