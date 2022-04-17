9f20cba482c1f9e158f3b18e77321347b033918d
nardost <nardos.tessema@gmail.com>
2022-04-16 19:38:33 -0500

    Some rocket details shown in Rocket component

--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,10 +1,48 @@
-import { Card } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Divider,
+  Paper,
+  Table,
+  TableContainer,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@mui/material";
 
 const Rocket = ({ details }) => {
     return (
-        <Card>
-            <div>{details.name}</div>
-        </Card>
+      <Card sx={{ minWidth: 300 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {details.name}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {details.country}
+          </Typography>
+          <Divider />
+          <TableContainer component={Paper}>
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    Height: {details.height.meters + "m, "}
+                    Diameter: {details.diameter.meters + "m, "}
+                    Mass: {details.mass.kg + "kg"}
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>First Flight: {details.first_flight}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Stages: {details.stages}, Landing legs: {details.landing_legs.number}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </CardContent>
+      </Card>
     );
 };
 
